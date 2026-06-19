a5014a66fe95fb5dc73aa594ac9b7fe6ebb36ee4
Asher Samuel <ashersam116@gmail.com>
2026-06-19 18:20:04 +0300

    support for logging and tracing

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
 	github.com/stretchr/testify v1.11.1
 	github.com/tysonmote/gommap v0.0.3
+	go.opencensus.io v0.24.0
+	go.uber.org/zap v1.18.1
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260226221140-a57be14db171
 	google.golang.org/grpc v1.81.1
 	google.golang.org/protobuf v1.36.11
@@ -17,7 +19,11 @@ require (
 	cloud.google.com/go/compute/metadata v0.9.0 // indirect
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,20 +2,38 @@ package server
 
 import (
 	"context"
+	"flag"
 	"net"
 	"os"
 	"testing"
+	"time"
 
 	api "github.com/devasherr/prolog/api/v1"
 	"github.com/devasherr/prolog/config"
 	"github.com/devasherr/prolog/internal/auth"
 	"github.com/devasherr/prolog/internal/log"
 	"github.com/stretchr/testify/require"
+	"go.opencensus.io/examples/exporter"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 )
+
+var debug = flag.Bool("debug", false, "Enable observability for debugging")
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	if *debug {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			panic(err)
+		}
+		zap.ReplaceGlobals(logger)
+	}
+	os.Exit(m.Run())
+}
 
 func TestServer(t *testing.T) {
 	for scenario, fn := range map[string]func(
@@ -47,8 +65,6 @@ func setupTest(t *testing.T, fn func(*Config)) (api.LogClient, api.LogClient, *C
 	clientCreds := credentials.NewTLS(clientTLSConfig)
 	grpc.Dial(l.Addr().String(), grpc.WithTransportCredentials(clientCreds))
 
-	// clientOptions := []grpc.DialOption{grpc.WithInsecure()}
-	// cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
 	require.NoError(t, err)
 
 	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
@@ -79,8 +95,8 @@ func setupTest(t *testing.T, fn func(*Config)) (api.LogClient, api.LogClient, *C
 		return conn, client, opts
 	}
 
-	_, rootClient, _ := newClient(config.RootClientCertFile, config.RootClientKeyFile)
-	_, nobodyClient, _ := newClient(config.NobodyClientCertFile, config.NobodyClientKeyFile)
+	rootConn, rootClient, _ := newClient(config.RootClientCertFile, config.RootClientKeyFile)
+	nobodyConn, nobodyClient, _ := newClient(config.NobodyClientCertFile, config.NobodyClientKeyFile)
 
 	require.NoError(t, err)
 	serverCreds := credentials.NewTLS(serverTLSConfig)
@@ -92,6 +108,23 @@ func setupTest(t *testing.T, fn func(*Config)) (api.LogClient, api.LogClient, *C
 	require.NoError(t, err)
 
 	authorizer := auth.New(config.ACLModelFile, config.ACLPolicyFile)
+	var telemetryExporter *exporter.LogExporter
+	if *debug {
+		metricsLogFile, err := os.CreateTemp("", "metrics-*.log")
+		require.NoError(t, err)
+		t.Logf("metrics log file: %s", metricsLogFile.Name())
+		tracesLogFile, err := os.CreateTemp("", "traces-*.log")
+		require.NoError(t, err)
+		t.Logf("traces log file: %s", tracesLogFile.Name())
+		telemetryExporter, err = exporter.NewLogExporter(exporter.Options{
+			MetricsLogFile:    metricsLogFile.Name(),
+			TracesLogFile:     tracesLogFile.Name(),
+			ReportingInterval: time.Second,
+		})
+		require.NoError(t, err)
+		err = telemetryExporter.Start()
+		require.NoError(t, err)
+	}
 	cfg := &Config{CommitLog: clog, Authorizer: authorizer}
 	if fn != nil {
 		fn(cfg)
@@ -106,9 +139,14 @@ func setupTest(t *testing.T, fn func(*Config)) (api.LogClient, api.LogClient, *C
 
 	return rootClient, nobodyClient, cfg, func() {
 		server.Stop()
-		// cc.Close()
+		rootConn.Close()
+		nobodyConn.Close()
 		l.Close()
-		clog.Remove()
+		if telemetryExporter != nil {
+			time.Sleep(1500 * time.Millisecond) // time for telemetry to flush data to disk
+			telemetryExporter.Stop()
+			telemetryExporter.Close()
+		}
 	}
 }
 
